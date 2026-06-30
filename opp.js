1445a7aedad181feda4273ca6606b80e60f849ab
RohanChoudhary123 <choudhryrohan80@gmail.com>
2026-06-30 12:41:58 +0530

    opp.js

---
+++ opp.js
@@ -0,0 +1,44 @@
+// const student ={
+//     name:"rohan",
+//     attendance:0,
+
+//     isPresent: function(){
+//         this.attendance++;
+//         console.log(
+//             `${this.name} is presnt.Total Attendace = ${this.attendance} `
+//         );
+//     },
+// };
+
+// student.isPresent();
+
+// class student{
+//     constructor(name, attendance){
+//         this.name=  name;
+//         this.attendance = attendance;
+//     }
+//     isPresent(){
+//         this.attendance++;
+//         console.log(
+//             `${this.name} is present.Total Attendance = ${this.attendance}`
+//         );
+//     }
+// }
+// const student1 = new student("rohan",0);
+// const student2 = new student("piyush",0);
+
+// student1.isPresent();
+// student2.isPresent();
+
+// student2.isPresent();
+// student1.isPresent();
+class student {
+    static greet(){
+        static greet(){
+            
+        }
+    }
+}
+
+
+        
